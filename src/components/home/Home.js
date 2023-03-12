aee90a45f31208c26bb3fd037a96de3e7a468bd3
VeselaDzhuvtova <108488627+VeselaDzhuvtova@users.noreply.github.com>
2023-03-12 20:41:49 +0200

    Render latest games conditionally

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -20,46 +20,11 @@ const Home = () => {
             <img src="./images/four_slider_img01.png" alt="hero" />
             <div id="home-page">
                 <h1>Latest Games</h1>
-                {/* Display div: with information about every game (if any) */}
-                    {games.map(x => <LatestGame game={x}/>)}
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/ZombieLang.png" />
-                    </div>
-                    <h3>Zombie Lang</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/MineCraft.png" />
-                    </div>
-                    <h3>MineCraft</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                {/* Display paragraph: If there is no games  */}
-                <p className="no-articles">No games yet</p>
+
+                {games.length > 0
+                ? games.map(x => <LatestGame key={x._id} game={x}/>)
+                : <p className="no-articles">No games yet</p>
+                }
             </div>
         </section>
     );
