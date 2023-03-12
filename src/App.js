364f6b21b5d1d0432180e9a7a76fad4cf800e41d
VeselaDzhuvtova <108488627+VeselaDzhuvtova@users.noreply.github.com>
2023-03-12 21:30:47 +0200

    Add react router to project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
@@ -8,7 +10,9 @@ function App() {
       <Header />
       {/* Main Content */}
       <main id="main-content">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </main>
       {/*Home Page*/}
       {/* Login Page ( Only for Guest users ) */}
